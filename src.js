50f001ebd79696fcf534b70aed175be8e903f0d2
Mayank Srivastav <mayanksri18@yahoo.in>
2017-09-28 10:26:52 +0530

    added initial user static data

--- src.js
+++ src.js
@@ -1,1 +1,89 @@
 
+const ownerUserData = {
+  ownerUser: "mayank",
+  images: [
+    {
+      id: 0,
+      url:
+        "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 2,
+      like: ["mayank", "dave"],
+      comments: [
+        { mayank: "Nice image" },
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis", "dave"]
+    },
+    {
+      id: 1,
+      url:
+        "https://images.pexels.com/photos/9352/glass-time-watch-business.jpg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 3,
+      like: ["mayank", "dave", "dennis"],
+      comments: [
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis"]
+    },
+    {
+      id: 2,
+      url:
+        "https://images.pexels.com/photos/552598/pexels-photo-552598.jpeg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 2,
+      like: ["mayank", "dave"],
+      comments: [
+        { mayank: "Nice image" },
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis", "dave"]
+    },
+    {
+      id: 3,
+      url:
+        "https://images.pexels.com/photos/236900/pexels-photo-236900.jpeg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 2,
+      like: ["mayank", "dave"],
+      comments: [
+        { mayank: "Nice image" },
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis", "dave"]
+    },
+    {
+      id: 4,
+      url:
+        "https://images.pexels.com/photos/280250/pexels-photo-280250.jpeg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 2,
+      like: ["mayank", "dave"],
+      comments: [
+        { mayank: "Nice image" },
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis", "dave"]
+    },
+    {
+      id: 5,
+      url:
+        "https://images.pexels.com/photos/8592/pexels-photo.jpg?h=350&w=350&auto=compress&cs=tinysrgb",
+      likes: 2,
+      like: ["mayank", "dave"],
+      comments: [
+        { mayank: "Nice image" },
+        { dave: "Great watch" },
+        { mayank: "Nice image again" },
+        { dennis: "Beautiful bezel" }
+      ],
+      fav: ["dennis", "dave"]
+    }
+  ]
+};
